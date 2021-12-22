d3df306584c85cbb6e19336be42e4f62443a2ee6
EarthT0Drew <drewschloesser8@gmail.com>
2021-12-22 16:07:53 -0700

    Create method 'ask'

--- maker.py
+++ maker.py
@@ -1,3 +1,17 @@
 class Maker:
     def __init__(self):
         self.questions = {}
+
+    def ask(self):
+        question = input("What do you want your question to be?")
+        answers = []
+
+        inputting_answers = True
+        while inputting_answers:
+            answer = input("What do you want your answer for that question to be? "
+                           "There can be more than one- Type '/Done' when finished")
+
+            if answer.lower() == "/done":
+                inputting_answers = False
+            else:
+                answers.append(answer)

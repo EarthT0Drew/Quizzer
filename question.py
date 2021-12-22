6550a2c5797347d0aea021e99b0a1dabc8f15d8e
EarthT0Drew <drewschloesser8@gmail.com>
2021-12-22 16:24:29 -0700

    Create class Question

--- question.py
+++ question.py
@@ -1,1 +1,10 @@
+class Question:
+    def __init__(self, question, answers):
+        self.question = question
+        self.answers = answers
+
+        self.answerers = {}
+
+    def add_answerer(self, user_name, user_id, answer, is_correct):
+        self.answerers[f"{user_name} ({user_id})"] = [answer, is_correct]
 
